419e790811cfa01c8110e71427a322b4fa3f7546
Francine <francine.godoi@gmail.com>
2025-01-31 15:45:28 -0300

    adicionado persistencia de dados, historico chat

--- database.py
+++ database.py
@@ -1,30 +1,17 @@
-import json
+import pathlib
+
+def salvar_historico_chat(nome_chat: str, dados):
+
+    with open(f'historico_chat/{nome_chat}.txt', 'a', encoding='utf-8') as arquivo:
+        arquivo.write(f'{dados}\n')
 
 
-def pegar_path_banco(db):
-    if db == 'clientes_conectados':
-        db_path = 'db/clientes_conectados.json'
-    elif db == 'salas':
-        db_path = 'db/salas.json'
-    elif db == 'moderadores':
-        db_path = 'db/moreradores.json'
-    return db_path
+def pegar_historico_chat(nome_chat: str):
+    with open(f'historico_chat/{nome_chat}.txt', 'r', encoding='utf-8') as arquivo:
+        dados = arquivo.readlines()
+    
+    return ''.join(f'\t{dado}' for dado in dados)
 
-
-def salvar_dados_banco(db: str, dados):
-    db_path = pegar_path_banco(db)
-    #teste = pegar_dados_banco(db)    
-    with open(db_path, mode='w', encoding='utf-8') as arquivo:
-        json.dump(dados, arquivo)
-
-
-def pegar_dados_banco(db: str):
-    db_path = pegar_path_banco(db)
-    try:
-        with open(db_path, mode='r', encoding='utf-8') as arquivo:
-            dados = json.load(arquivo)
-            return dados
-    except json.JSONDecodeError:
-        return None
-
-
+def limpar_historico():
+    for arquivo in pathlib.Path('historico_chat').iterdir():
+        arquivo.unlink(missing_ok=True)
